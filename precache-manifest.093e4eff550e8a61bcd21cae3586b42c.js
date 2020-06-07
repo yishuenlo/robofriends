ef1f49ab432fae23d52c7eca25564fab0face6a3
yishuenlo <lo.yishuen@gmail.com>
2020-06-07 13:04:48 -0600

    Updates

---
+++ precache-manifest.093e4eff550e8a61bcd21cae3586b42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d9962ed3cd6cab231069facaea705c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "70f50a959d19b699b69f",
+    "url": "/robofriends/static/css/main.088a9884.chunk.css"
+  },
+  {
+    "revision": "4e908bd127622a073107",
+    "url": "/robofriends/static/js/2.1c970a8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.1c970a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f50a959d19b699b69f",
+    "url": "/robofriends/static/js/main.ba9e3aa6.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
